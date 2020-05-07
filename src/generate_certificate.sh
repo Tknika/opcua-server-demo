0e6493b595a697410d28a8b39cc2505676f2e7ab
Aitor Iturrioz <riturrioz@gmail.com>
2020-05-07 23:07:44 +0200

    Check if certs exit before generating new ones
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/generate_certificate.sh
+++ src/generate_certificate.sh
@@ -1,3 +1,28 @@
-openssl genrsa -out key.pem 2048
-openssl req -x509 -days 365 -new -out certificate.pem -key key.pem -config ssl.conf
-openssl x509 -outform der -in certificate.pem -out certificate.der
+#!/bin/bash 
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_FOLDER=$(dirname "$SCRIPT_PATH")
+
+KEY_FILE_PATH="$SCRIPT_FOLDER/key.pem"
+SSL_FILE_PATH="$SCRIPT_FOLDER/ssl.conf"
+CERTIFICATE_PEM_FILE_PATH="$SCRIPT_FOLDER/certificate.pem"
+CERTIFICATE_DER_FILE_PATH="$SCRIPT_FOLDER/certificate.der"
+
+if [ -f $KEY_FILE_PATH ]; then
+    echo "$KEY_FILE_PATH file already exists!"
+    exit
+fi
+
+if [ -f $CERTIFICATE_DER_FILE_PATH ]; then
+    echo "$CERTIFICATE_DER_FILE_PATH file already exists!"
+    exit
+fi
+
+if [ ! -f $SSL_FILE_PATH ]; then
+    echo "$SSL_FILE_PATH does not exists!"
+    exit
+fi
+
+openssl genrsa -out $KEY_FILE_PATH 2048
+openssl req -x509 -days 365 -new -out $CERTIFICATE_PEM_FILE_PATH -key $KEY_FILE_PATH -config $SSL_FILE_PATH
+openssl x509 -outform der -in $CERTIFICATE_PEM_FILE_PATH -out $CERTIFICATE_DER_FILE_PATH
